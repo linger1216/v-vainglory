50a7fabccf8e4a1df3a957e4eae96db2f61d7d39
lid.guan <lid.guan@gmail.com>
2025-02-24 09:43:49 +0800

    restruct project

--- src/main/java/com/vainglory/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.vainglory.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.vainglory.pojo.User;
-import org.apache.ibatis.annotations.Mapper;
-
-
-public interface UserMapper extends BaseMapper<User> {
-}
